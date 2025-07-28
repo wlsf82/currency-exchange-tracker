3357141adcb49ca5080b2b50435dde60ac8d0935
Walmyr <wlsf82@gmail.com>
2025-07-28 16:41:38 +0200

    Improve currency buttons and checkboxes style
    So they are centered also on smaller viewports.
    
    Also, force a cache update.

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'currency-tracker-v15';
-const API_CACHE_NAME = 'currency-api-cache-v15';
+const CACHE_NAME = 'currency-tracker-v16';
+const API_CACHE_NAME = 'currency-api-cache-v16';
 const urlsToCache = [
   './index.html',
   './app.js',
