824f157b541cb762e04278b68c2f27e37a97fe48
Walmyr <wlsf82@gmail.com>
2026-07-07 18:26:04 +0200

    Move the status also

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'currency-tracker-v23';
-const API_CACHE_NAME = 'currency-api-cache-v23';
+const CACHE_NAME = 'currency-tracker-v24';
+const API_CACHE_NAME = 'currency-api-cache-v24';
 const urlsToCache = [
   './index.html',
   './app.js',
